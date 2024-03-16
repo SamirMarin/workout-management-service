8184dea611ea845a1c55af4c19ef6c5931020295
Samir Marin <marin.samir@gmail.com>
2024-03-15 19:55:15 -0700

    Use aws cli to check dynamodb local is ready

--- scripts/dynamodb/create-table.sh
+++ scripts/dynamodb/create-table.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Waiting for DynamoDB Local to be ready..."
+# Wait for DynamoDB Local to become ready
+until aws dynamodb list-tables --endpoint-url http://localhost:8000 --region us-west-2 > /dev/null 2>&1; do
+    echo "DynamoDB Local is not ready yet..."
+    sleep 5
+done
+echo "DynamoDB Local is ready."
 # Command to create a DynamoDB table
 # Specifying ReadCapacityUnits and WriteCapacityUnits is required in local mode
 aws dynamodb create-table \
